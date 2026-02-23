52168ea3248bc449325d2daaeb77335e4df669c3
umair98391 <umair98391@gmail.com>
2026-02-23 06:07:29 +0530

    Update app.py

--- app.py
+++ app.py
@@ -7,3 +7,12 @@ app = FastAPI()
 @app.get("/")
 def read_root():
     return {"message": "Hello, FastAPI is working!"}
+
+
+# This makes the file executable directly
+if __name__ == "__main__":
+    uvicorn.run(
+        "main:app",      # filename:app_variable
+        host="0.0.0.0",  # accessible from network
+        port=8000
+    )
